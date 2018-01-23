2c2d34f55cc0103f6c9075bc9d59203b26b67025
PerfectStorm85 <info@ronkamphuis.nl>
2018-01-23 23:22:06 +0100

    Update DEPLOY_MAVEN-rocksdb-armv7.sh

--- scripts/rocksdb/DEPLOY_MAVEN-rocksdb-armv7.sh
+++ scripts/rocksdb/DEPLOY_MAVEN-rocksdb-armv7.sh
@@ -3,7 +3,7 @@
 VER=5.9.2
 
 M2_ROCKSDBJNI=$HOME/.m2/repository/org/rocksdb/rocksdbjni/$VER
-JARBALL=rocksdb/java/target/rocksdbjni-$VER-linux64.jar
+JARBALL=java/target/rocksdbjni-$VER-linux64.jar
 
 set -x
 
@@ -12,5 +12,5 @@ mkdir -p $M2_ROCKSDBJNI
 cp $JARBALL $M2_ROCKSDBJNI/rocksdbjni-$VER.jar
 sha1sum $JARBALL | cut -f1 -d ' ' > $M2_ROCKSDBJNI/rocksdbjni-$VER.jar.sha1
 
-cp rocksdb/java/rocksjni.pom $M2_ROCKSDBJNI/rocksjni-$VER.pom
-sha1sum rocksdb/java/rocksjni.pom | cut -f1 -d ' ' > $M2_ROCKSDBJNI/rocksdbjni-$VER.pom.sha1
+cp java/rocksjni.pom $M2_ROCKSDBJNI/rocksjni-$VER.pom
+sha1sum java/rocksjni.pom | cut -f1 -d ' ' > $M2_ROCKSDBJNI/rocksdbjni-$VER.pom.sha1
